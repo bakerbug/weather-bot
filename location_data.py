a38817a05f381ad7aeaa7647395430a874924c8e
Bill Baker <bill.baker@synapsewireless.com>
2018-05-18 10:04:08 -0500

    More cleanup.  Adjusted sleep timers to avoid TOS violation.

--- location_data.py
+++ location_data.py
@@ -2,11 +2,15 @@ def get_location(place='home'):
     locations = {
         'home': {'lat': 34.718553, 'lon': -86.778150, 'label': 'Madison, AL'},
         'mom': {'lat': 34.807048, 'lon': -92.282116, 'label': 'North Little Rock, AR'},
-        'test': {'lat': 31.097370, 'lon': -87.872430, 'label': 'Some random place with interesting weather'},
+        'test': {'lat': 64.820051, 'lon': -147.626587, 'label': 'Some random place with interesting weather'},
     }
 
-    lat = locations[place]['lat']
-    lon = locations[place]['lon']
-    label = locations[place]['label']
+    try:
+        lat = locations[place]['lat']
+        lon = locations[place]['lon']
+        label = locations[place]['label']
+    except KeyError:
+        print('*** Location not recognized. ***')
+        exit()
 
     return lat, lon, label
