b23455d54235156c2d3bc999cd9ae3945e158fd8
Bill Baker <bill.baker@synapsewireless.com>
2018-05-19 17:32:04 -0500

    Adds Wemo support.  Additional cleanup.

--- location_data.py
+++ location_data.py
@@ -2,7 +2,7 @@ def get_location(place='home'):
     locations = {
         'home': {'lat': 34.718553, 'lon': -86.778150, 'label': 'Madison, AL'},
         'mom': {'lat': 34.807048, 'lon': -92.282116, 'label': 'North Little Rock, AR'},
-        'test': {'lat': 64.820051, 'lon': -147.626587, 'label': 'Some random place with interesting weather'},
+        'test': {'lat': 35.745894, 'lon': -89.531717, 'label': 'Some random place with interesting weather'},
     }
 
     try:
--- weather_bot.py
+++ weather_bot.py
@@ -1,33 +1,64 @@
-import wemo
+import pywemo as wemo
 import argparse
 from darksky import forecast
 from datetime import date, timedelta
 from credentials import darksky_key as api_key
 from location_data import get_location
-from time import sleep
+from time import time, localtime, sleep, asctime
 
 active_alerts = 0
+lights = []
 CAUTION_TYPES = ('Tornado Warning', 'Tornado Watch', 'Severe Thunderstorm Warning', 'Severe Thunderstorm Watch', 'Wind Advisory')
-ALERT_TYPES = ('Tornado Warning', 'Wind Advisory')
+ALERT_TYPES = ('Tornado Warning',)
+LIGHTS_CONTROLLED = ('Sofa Lamp', 'Entry Lamp', 'Bedroom Lamp')
 
 # CONFIGURATION #
 # Sleep time between polls
 SLEEP_CLEAR = (60 * 15)  # Every 15 minutes
 SLEEP_STORMY = 60  # Once a minute
 
+def prepare_lights():
+    """ Discovers all Wemo lights, and stores data for those to be controlled."""
+    global lights
+
+    # Lights are already prepared, don't waste time doing it again.
+    if len(lights) > 0:
+        return
+
+    discovered_lights = wemo.discover_devices()
+
+    if DEBUG:
+        for light in discovered_lights:
+            print('Discovered ' + light.name)
+
+    for light in discovered_lights:
+        if light.name in LIGHTS_CONTROLLED:
+            lights.append(light)
+
+def forget_lights():
+    """Clears discovered lights so that the data doesn't become stale."""
+    global lights
+    lights = []
+
 
 def activate_warning():
     if DEBUG is True:
         print('### TORNADO DETECTED ###')
+    for light in lights:
+        light.on()
+        if DEBUG:
+            print('Enabled ' + light.name)
 
 
-def monitor_alerts(alerts):
+def check_alerts(alerts):
+    """ Inspects weather data for alerts and takes action for certain conditions. """
     global active_alerts
 
     active_alerts = 0
     for alert in alerts:
         if alert['title'] in CAUTION_TYPES:
             active_alerts += 1
+            prepare_lights()
             if DEBUG is True:
                 print('Alert ' + str(active_alerts) + ':  ' + alert['title'])
             if alert['title'] in ALERT_TYPES:
@@ -37,9 +68,12 @@ def monitor_alerts(alerts):
         print('##############################')
 
 
-def check_weather(lat, lon, location):
-    global active_alerts
-
+def get_weather_statements(lat, lon, location):
+    """ Fetches the weather data.
+    lat:  Latitude coordinates
+    lon:  Longitude coordinates
+    location:  A description of the location"
+    """
     weekday = date.today()
 
     #with forecast(darksky_key, lat, lon) as current_forecast:
@@ -47,7 +81,7 @@ def check_weather(lat, lon, location):
         current_forecast = forecast(api_key, lat, lon)
     except:
         print('Unable to retrieve weather data.')
-        return
+        return None
 
     if ALL_OUTPUT is True:
         print('Report for: ' + location)
@@ -64,13 +98,14 @@ def check_weather(lat, lon, location):
         print('---')
 
     try:
-        active_alerts = len(current_forecast['alerts'])
-        monitor_alerts(current_forecast['alerts'])
+        active_statements = len(current_forecast['alerts'])
+        return current_forecast['alerts']
     except KeyError:
-        return
+        return None
 
 
 def arg_parser():
+    """ Sets command line arguments as global variables. """
     global DEBUG, RUN_ONCE, ALL_OUTPUT, LOCATION
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--once', help='Only run once', action='store_true')
@@ -91,11 +126,15 @@ if __name__ == '__main__':
 
     current_lat, current_lon, current_name = get_location(LOCATION)
     while True:
-        check_weather(current_lat, current_lon, current_name)
+        weather_statements = get_weather_statements(current_lat, current_lon, current_name)
+
+        if weather_statements is not None:
+            check_alerts(weather_statements)
 
         if DEBUG is True:
+            now = localtime(time())
             poll_count += 1
-            print('Poll count: ' + str(poll_count))
+            print('Poll count: ' + str(poll_count) + ' at ' + asctime(now))
 
         if RUN_ONCE is True:
             break
